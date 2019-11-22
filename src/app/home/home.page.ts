db5dc30ff8e11372e7389e3de909094e2cf9704c
Soham <soham.patel@bugzstudio.com>
2019-11-22 18:37:31 +0530

    Manage Numeric keyboard

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -26,7 +26,29 @@ export class HomePage {
     this.total = 0.00
   }
 
+  InputNum(Num) {
+    if (Num == 0) {
+      this.total = 0.00
 
+    }
+    else {
+      var num;
+
+      if (this.total > 0) {
+        num = this.total.toString() + Num.toString();
+      }
+      else {
+        num = Num;
+      }
+
+      this.total = (num);
+    }
+  }
+  FloatTotal(Num) {
+
+    return parseInt(Num) / 100;
+
+  }
   async presentAlertMultipleButtons(i) {
     const alert = await this.alertController.create({
       header: 'Alert',
@@ -42,13 +64,13 @@ export class HomePage {
 
 
     // start camera
-  /*  this.cameraPreview.startCamera(cameraPreviewOpts).then(
-      (res) => {
-        console.log(res)
-      },
-      (err) => {
-        console.log(err)
-      });*/
+    /*  this.cameraPreview.startCamera(cameraPreviewOpts).then(
+        (res) => {
+          console.log(res)
+        },
+        (err) => {
+          console.log(err)
+        });*/
 
     /*let successCallback = (isAvailable) => { console.log('Is available? ' + isAvailable); }
     let errorCallback = (e) => console.error(e);
@@ -97,103 +119,103 @@ export class HomePage {
 }
 
 
-  /*ngOnInit() {
-    var uid = 123
-    var store = 456
-    var user_points = 10
-    var unlocked_items = []
+/*ngOnInit() {
+  var uid = 123
+  var store = 456
+  var user_points = 10
+  var unlocked_items = []
 
-    var userUnlocks = [
-      {
-        title: 'test',
-        id: '1',
-        user: '123',
-        store: '456',
-      },
-      {
-        title: 'test2',
-        id: '2',
-        user: '123',
-        store: '456'
-      },
-      {
-        title: 'test3',
-        id: '3',
-        user: '123',
-        store: '456'
-      },
-      {
-        title: 'test4',
-        id: '8',
-        user: '123',
-        store: '456'
-      },
+  var userUnlocks = [
+    {
+      title: 'test',
+      id: '1',
+      user: '123',
+      store: '456',
+    },
+    {
+      title: 'test2',
+      id: '2',
+      user: '123',
+      store: '456'
+    },
+    {
+      title: 'test3',
+      id: '3',
+      user: '123',
+      store: '456'
+    },
+    {
+      title: 'test4',
+      id: '8',
+      user: '123',
+      store: '456'
+    },
 
-    ]
+  ]
 
-    var store_rewards = [
-      {
-        title: 'test',
-        id: '1',
-        user: '123',
-        store: '456',
-        points_to_unlock: 5
-      },
-      {
-        title: 'test1',
-        id: '2',
-        user: '123',
-        store: '456',
-        points_to_unlock: 6
-      },
-      {
-        title: 'test2',
-        id: '3',
-        user: '123',
-        store: '456',
-        points_to_unlock: 7
-      },
-      {
-        title: 'test3',
-        id: '4',
-        user: '123',
-        store: '456',
-        points_to_unlock: 8
-      },
-      {
-        title: 'test4',
-        id: '5',
-        user: '123',
-        store: '456',
-        points_to_unlock: 15
-      },
-      {
-        title: 'test5',
-        id: '6',
-        user: '123',
-        store: '456',
-        points_to_unlock: 15
-      },
+  var store_rewards = [
+    {
+      title: 'test',
+      id: '1',
+      user: '123',
+      store: '456',
+      points_to_unlock: 5
+    },
+    {
+      title: 'test1',
+      id: '2',
+      user: '123',
+      store: '456',
+      points_to_unlock: 6
+    },
+    {
+      title: 'test2',
+      id: '3',
+      user: '123',
+      store: '456',
+      points_to_unlock: 7
+    },
+    {
+      title: 'test3',
+      id: '4',
+      user: '123',
+      store: '456',
+      points_to_unlock: 8
+    },
+    {
+      title: 'test4',
+      id: '5',
+      user: '123',
+      store: '456',
+      points_to_unlock: 15
+    },
+    {
+      title: 'test5',
+      id: '6',
+      user: '123',
+      store: '456',
+      points_to_unlock: 15
+    },
 
-    ]
+  ]
 
-    for (let i of store_rewards) {
-      if (user_points >= i.points_to_unlock) {
-        unlocked_items.push(i)
-      }
+  for (let i of store_rewards) {
+    if (user_points >= i.points_to_unlock) {
+      unlocked_items.push(i)
     }
-
-    //console.log(unlocked_items)
-
-    var result = userUnlocks.filter(function(o1) {
-      // filter out (!) items in result2
-      return !unlocked_items.some(function(o2) {
-        return o1.id === o2.id; // assumes unique id
-      });
-    });
-    console.log(result)
-    //console.log(unlocked_items)
   }
+
+  //console.log(unlocked_items)
+
+  var result = userUnlocks.filter(function(o1) {
+    // filter out (!) items in result2
+    return !unlocked_items.some(function(o2) {
+      return o1.id === o2.id; // assumes unique id
+    });
+  });
+  console.log(result)
+  //console.log(unlocked_items)
+}
 
 
 }*/
