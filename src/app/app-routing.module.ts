9002ccb05b6111a70856fd6861db059bf8dbffa7
Davin Glick <info@pixel-lab.co>
2019-12-04 13:34:48 +1100

    added qr page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'data', redirectTo: 'data-filter', pathMatch: 'full' },
-  { path: '', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'data-filter', loadChildren: './data-filter/data-filter.module#DataFilterPageModule' },
+  { path: '', loadChildren: './qr-scanner/qr-scanner.module#QrScannerPageModule' },
 ];
 
 @NgModule({
